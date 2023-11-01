7c442d8b076426ddbd925ab0fd524d6d251858b5
Evan Losh <evand.losh@gmail.com>
2023-11-01 02:52:55 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(namesArray, filterString) {
+    return namesArray.filter((x) => {
+        return x.toLowerCase() === filterString.toLowerCase();
+    });
+}
+
+function fuzzyMatch(namesArray, filterString) {
+    return namesArray.filter((x) => {
+        return x.substring(0, filterString.length).toLowerCase() === filterString.toLowerCase();
+    });
+}
+
+function matchName(objectsArray, filterString) {
+    // The objects in objectsArray have keys called name and hometown
+    return objectsArray.filter((x) => {
+        return x.name.toLowerCase() === filterString.toLowerCase();
+    });
+}
